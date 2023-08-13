d2804fa7037d0f84a0eaafd475555d13ffc2a806
Kunal <79614769+Kunal-Kokate@users.noreply.github.com>
2023-08-12 21:06:48 -0400

    Add files via upload

---
+++ Madlib.py
@@ -0,0 +1,16 @@
+name = input("Enter a name: ")
+adj = input("Enter an adjective: ")
+emotion = input("Enter an emotion: ")
+verb = input("Enter a verb ending in -ing: ")
+adj2 = input("Enter another adjective: ")
+name2 = input("Enter a name: ")
+adj3 = input("Enter another adjective: ")
+verb2 = input("Enter a verb ending in -ing: ")
+
+
+
+madlib = f"In the world of swashbuckling pirates, Captain {name} was known for {adj} antics that always left the crew {emotion}. Whether it was {verb} \
+with seagulls or {adj2} dance-offs on the deck, Captain {name2} turned every voyage into a(n) {adj3} escapade that had even the most stoic sailors {verb2} \
+with laughter."
+
+print(madlib)
